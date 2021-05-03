e4b551d93fb2cb05882bdbbee850a527ecf6237e
ldh <kura1031@naver.com>
2021-05-03 12:27:48 +0900

    Add .gitignore

---
+++ JavaEx/src/com/javaex/basics/OperEx.java
@@ -0,0 +1,116 @@
+package com.javaex.basics;
+
+public class OperEx {
+
+	public static void main(String[] args) {
+		arithOperEx();
+		logicOper();
+		bitOper();
+	}
+	
+	// 비트 연산자
+	// 비트 단위로 미세한 조작이 필요할 때 사용
+	// 하드웨어 제거, 이미지 프로세싱
+	private static void bitOper() {
+		byte b1 = 0b1101;
+		byte b2 = 0b0111;
+		
+		System.out.println("b1:" + Integer.toBinaryString(b1));
+		System.out.println("b2:" + Integer.toBinaryString(b2));
+		int result = b1 & b2; // 비트 논리 곱: 둘 다 1이어야 1비트로 세팅
+		System.out.println("b1%b2: " + Integer.toBinaryString(result));
+		result = b1 | b2; // 비트 논리 합: 둘 중 한 개만 1이면 1비트 세팅
+		System.out.println("b1|b2: " + Integer.toBinaryString(result));
+		result = ~b1; // 비트 논리 부정 : 1 <-> 0
+		System.out.println("~b1: " + Integer.toBinaryString(result));
+		
+	}
+	//비교연산자, 논리연산자
+	// 결과로 boolean 반환, 논리값으로 프로그램의 흐름 제어 -> 중요
+	private static void logicOper() {
+		// 비교 연산자 >, >=, <, <=, ==, !=
+		int n1 = 7;
+		int n2 = 3;
+		
+		System.out.println("a가 b와 같은가? " + (n1 == n2));
+		System.out.println("a가 b와 같지 않은가? " + (n1 != n2));
+		
+		// 논리 연산 : AND(논리곱: &&), OR(논리합: ||), NOT(논리부정: !)
+		// -> 집합을 떠올리자
+		int n3 = 5;
+		
+		// n3는 0초과, 10미만의 값인가?
+		// 조건 1: n3 > 0
+		// 조건 2: n3 < 10
+		// -> 조건1 and 조건2 : 교집합 영역
+		boolean r1 = n3 > 0; // 조건1
+		boolean r2 = n3 < 10; //조건2
+		boolean r1andr2 = r1 && r2; // n3 > 0 && n3 < 10
+		System.out.println("n3가 0 초과 10 미만 영역에 있는가? " + r1andr2);
+		
+		// n3는 0 이하이거나 10이상의 값인가?
+		// 조건 1: n3 <= 0
+		// 조건 2: n3 >= 10
+		// -> 조건1 or 조건2 : 합집합 영역
+		r1 = n3 <= 0;
+		r2 = n3 >= 10;
+		boolean r1orr2 = r1 || r2;
+		System.out.println("n3가 0 이하 10 이상 영역에 있는가? " + r1orr2);
+		
+		// not 논리 부정 -> true <-> false 반전
+		boolean rNot = !(n3 > 0 && n3 < 10); // 여집합 영역
+		// -> n3 <= 0 || n3 >= 10
+		System.out.println("논리부정: " + rNot);
+		
+	}
+	
+	//산술연산자
+	private static void arithOperEx() {
+		int a = 7;
+		int b = 3;
+		
+		// 부호연산자: +, -
+		System.out.println(-a); // a * -1
+		
+		// 사칙연산: +, -, *, /
+		System.out.println(a + "/" + b + "=" + (a / b)); //정수 나눗셈의 몫
+		System.out.println("a를 b로 나눈 나머지: " + (a % b)); //정수 나눗셈의 나머지
+		
+		// 정수 / 정수 -> 정수
+		
+		// 실제 나눗셈의 결과는?
+		System.out.println((float)a / (float)b);  // int -> float형으로 변환
+		System.out.println((float)a / b);  // b는 float로 자동변환(float형이 int형 보다 범위가 넓기 떄문에!)
+		
+		System.out.println("-------------------");
+		// 증감연산자 ++,--
+		// 위치에 따라 연산순서가 달라진다
+		a = 7;
+		System.out.println("a:"+ a);
+		System.out.println("전위 증감연산:" + (++a));
+		System.out.println("최종 a:" + a);
+		
+		
+		a = 7;
+		System.out.println("a:" + a);
+		System.out.println("후위 증감연산:"+ (a++));
+		System.out.println("최종 a:" + a);
+		
+		// 나눗셈 보충
+		a = 7;
+		// System.out.println(a / 0); -> 정수를 0으로 나누면 ArithmeticException
+		System.out.println((float)a / 0); // 실수를 0으로 나누면 ㄱㅊ Infinity
+		
+		
+		// Infinity가 포함된 연산식은 항상 Infinity
+		// Infinity의 체크
+		System.out.println("7.0 / 0은 유한수인가? " + Double.isFinite(7.0/0));
+		// Infinity가 포함된 연산식 -> Infinity
+		
+		System.out.println("0.0 / 0.0?" +  (0.0/0.0)); //Not A Number
+		// NaN가 포함된 연산식 -> NaN : 연산 불가의 의미
+		System.out.println("0.0 / 0.0 is NaN ? " + Double.isNaN(0.0/0.0));
+		System.out.println(0.0 / 0.0 + 10);
+		
+ 	}
+}
