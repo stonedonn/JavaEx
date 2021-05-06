a423ad90545c4fbca52cca2c30a504fe7088fbfb
ldh <kura1031@naver.com>
2021-05-06 13:47:15 +0900

    메서드, getter/setter!!!

--- JavaEx/src/com/javaex/oop/goods/v2/Goods.java
+++ JavaEx/src/com/javaex/oop/goods/v2/Goods.java
@@ -3,6 +3,27 @@ package com.javaex.oop.goods.v2;
 // v2. 정보 은닉
 public class Goods {
 	//  필드
+	
+	
 	private String name;
 	private int price;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name; // 두 개의 name 값이 혼란을 일으키기 때문에 this를 써준다
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	// 메서드
+	public void showInfo() {
+		System.out.println("상품명: " + name);
+		System.out.println("가격: " + price);
+	}
 }
--- JavaEx/src/com/javaex/oop/goods/v2/GoodsApp.java
+++ JavaEx/src/com/javaex/oop/goods/v2/GoodsApp.java
@@ -4,22 +4,27 @@ public class GoodsApp {
 	//에러
 	public static void main(String[] args) {
 		Goods notebook = new Goods();
-		
-		notebook.name = "LG Gram"; 
-		notebook.price = 1500000;
-		
+		// setter를 이용한 우회 설정
+//		notebook.name = "LG Gram"; 
+//		notebook.price = 1500000;
+		notebook.setName("LG Gram");
+		notebook.setPrice(1500000);
 		Goods smartphone = new Goods();
 		
-		smartphone.name="iPhone 12 pro";
-		smartphone.price=1000000;
-		
+//		smartphone.name="iPhone 12 pro";
+//		smartphone.price=1000000;
+		smartphone.setName("iPone 12 pro");
+		smartphone.setPrice(1000000);
 		System.out.printf("%s, %d원%n",
-				notebook.name,
-				notebook.price);
+				notebook.getName(),
+				notebook.getPrice());
 		System.out.printf("%s, %d원\n",
-				smartphone.name,
-				smartphone.price);
-
+				smartphone.getName(),
+				smartphone.getPrice());
+		
+		// 내장 메서드 호출
+		notebook.showInfo();
+		smartphone.showInfo();
 	}
 
 }
