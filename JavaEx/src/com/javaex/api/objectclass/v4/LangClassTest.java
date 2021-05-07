aea3133a39f3d7ac986e9ad87d33c272d0765267
이동현 <@DESKTOP-JJ1ODCJ>
2021-05-07 16:27:05 +0900

    깊은 복제

---
+++ JavaEx/src/com/javaex/api/objectclass/v4/LangClassTest.java
@@ -0,0 +1,28 @@
+package com.javaex.api.objectclass.v4;
+
+public class LangClassTest {
+
+	public static void main(String[] args) {
+		Scoreboard s1 = new Scoreboard(new int[] {10, 20, 30, 40}) ;
+		System.out.println("s1 = " + s1);
+		
+		
+		// 참조 복제
+		Scoreboard s2 = s1;
+		System.out.println("s1 == s2 ? " + (s1 == s2));
+		
+		// 얕은 복제
+		s2 = s1.getClone();
+		System.out.println("s2 = " + s2);
+		
+		// 확인
+		System.out.println("s1 == s2 ? " + (s1 == s2));
+		// 얕은 복제의 경우 참조 필드는 주소 값이 복사
+		System.out.println("s1.scores == s2.scores ? " 
+					+ (s1.getScores() == s2.getScores()));
+		s2.getScores()[2] = 100; // s2의 값을 바궜는데 참조 필드가 같아 s1의 값도 달라짐
+		System.out.println("s1 = " + s1);
+		System.out.println("s2 = " + s2);
+	}
+
+}
