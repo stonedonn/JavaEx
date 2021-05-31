53137c74a545f107e282cbe044f6e9310373ef99
이동현 <kura1031@naver.com>
2021-06-01 08:55:36 +0900

    Mini Project

---
+++ JavaEx/src/com/javaex/phonebook/PhoneBookDAO.java
@@ -0,0 +1,12 @@
+package com.javaex.phonebook;
+
+import java.util.List;
+
+
+public interface PhoneBookDAO {
+	public List<PhoneBookVO> getList(); // 단순 SELECT
+	public List<PhoneBookVO> search(String keyword); //LIKE
+	public PhoneBookVO get(Long id); // PK로 가져오기
+	public boolean insert(PhoneBookVO vo); //INSERT
+	public boolean delete(Long id); // PK로 삭제
+}
